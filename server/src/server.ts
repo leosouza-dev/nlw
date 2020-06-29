8300e17d0e0f9b03c174817494cc1e68ccaf6b77
leosouza-dev <sarival@sarival.com.br>
2020-06-29 12:46:00 -0300

    usando request param e metodo post

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,39 @@
-import express from 'express';
+import express, { json } from 'express';
 
 const app = express();
 
+const users = [
+    'Leonardo',
+    'Lari',
+    'Chicó',
+    'Loki',
+    'teste 1'
+];
+
+// retorna todos users
 app.get('/users', (request, response) => {
     console.log('listagem de usuários');
-    response.json([
-        'Leonardo',
-        'Lari',
-        'Chicó',
-        'Loki',
-        'teste'
-    ]);
+    return response.json(users);
+});
+
+// retorna um user (request param - id)
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Leonardo',
+        email: `leonardo.rodrigues@caelum.com.br`
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
 
-//npx ts-node src/server.ts   
+// npx ts-node src/server.ts   
+// npm run dev

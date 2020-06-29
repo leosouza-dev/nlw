1870a359b8d2cb8648f7e5da3b20d88f988fd229
leosouza-dev <sarival@sarival.com.br>
2020-06-29 15:10:01 -0300

    limpando o projeto

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,55 +3,8 @@ import express, { json, request } from 'express';
 const app = express();
 app.use(express.json());
 
-const users = [
-    'Leonardo',
-    'Lari',
-    'Chicó',
-    'Loki',
-    'teste 1'
-];
-
-// retorna todos users
-// app.get('/users', (request, response) => {
-//     console.log('listagem de usuários');
-//     return response.json(users);
-// });
-
-// retorna um user (request param - id)
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const user = users[id];
-
-    return response.json(user);
-});
-
-// http://localhost:3333/users?search=ardo
-// query param - fazendo filtro
-app.get('/users', (request, response) => {
-    const search = String(request.query.search); // recupera o 'ardo'
-
-    // encontra os users que contenham o 'ardo'
-    // usamos o=um ternario - se tiver algo filta, se nao devolve tudo
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-    return response.json(filteredUsers);
-});
-
-// testando o request param - vem do corpo da requisição
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    const user = {
-        name: data.name,
-        email: data.email
-    }
-    return response.json(user);
+app.get('/', (request, response) => {
+    return response.json({ message: 'Ola Mundo'});
 });
 
 app.listen(3333);
-
-// npx ts-node src/server.ts   
-// npm run dev
-
-// vamos usar knex para trabalhar com banco de dados relacionais
-// padrão - SELECT * FROM  users WHERE name = 'Chicó'
-// usando o Knex - knex('users').where.('name', 'Chicó').select(*)

ddb15c017c41811ffd53dd626d6a1c89f462780f
leosouza-dev <sarival@sarival.com.br>
2020-06-29 15:15:40 -0300

    isolando rotas em routes.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,9 @@
-import express, { json, request } from 'express';
+import express from 'express';
+import routes from './routes'
 
 const app = express();
-app.use(express.json());
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Ola Mundo'});
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);

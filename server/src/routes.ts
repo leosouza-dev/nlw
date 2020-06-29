bcef96dbd7cde0c2fe73fc70d74e76f6c2d00d1e
leosouza-dev <sarival@sarival.com.br>
2020-06-29 17:00:57 -0300

    criando a rota de listagem de items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Ola Mundo'});
+routes.get('/items', async (request, response) => {
+    // select * from items 
+    // como demora um pouco, usar await
+    const items = await knex('items').select('*');
+
+    return response.json(items);
 });
 
 export default routes;

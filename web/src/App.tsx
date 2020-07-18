ac6c55a5e4d3304602bee62c43e1509d0e397673
sarival <sarival@sarival.com.br>
2020-07-18 17:28:43 -0300

    aprendendo conceitos do react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react'; //importa o useSate
 import './App.css';
+import Header from './Header';
 
 function App() {
+  //cria uma variavel - "counter" e usa o useSate
+  //useState retorna um array [valor do estado, função para atualizar o valor do estado]
+  //então usamos a desestruturação para guardar esses valores
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick(){
+    //usa o metodo para atualozar o contador
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Olá Mundo!</h1>
-      </header>
+    <div>
+      <Header title={`Contador: ${counter}`} />
+
+    {/* exibe o valor do contador */}
+    <h1>{counter}</h1>
+    {/* botão chama a função para incrementar o valor do estado */}
+    <button onClick={handleButtonClick}>Clicar</button>
+
+      <h1>Conteudo da Aplicação</h1>
     </div>
   );
 }

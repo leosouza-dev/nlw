62d89fbcb060e982d76839bb0d258f5dc88ec21c
leosouza-dev <sarival@sarival.com.br>
2020-07-19 15:54:12 -0300

    criado roteamento

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'; //importa o useSate
+
 import './App.css';
-import Home from './pages/Home';
+import Routes from './routes';
 
 function App() {
   return (
-    <Home />
+    <Routes />
   );
 }
 
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -15,7 +15,7 @@ const Home = () => {
                 <main>
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
-                    <a href="/cadastro">
+                    <a href="/create-point">
                         <span>
                             <FiLogIn />
                         </span>

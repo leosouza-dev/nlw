592d11249938c1afc531134e358017e02b62da5c
sarival <sarival@sarival.com.br>
2020-07-19 13:17:06 -0300

    feito tela inicial

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,10 @@
-import React, { useState } from 'react'; //importa o useSate
+import React from 'react'; //importa o useSate
 import './App.css';
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  //cria uma variavel - "counter" e usa o useSate
-  //useState retorna um array [valor do estado, função para atualizar o valor do estado]
-  //então usamos a desestruturação para guardar esses valores
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick(){
-    //usa o metodo para atualozar o contador
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title={`Contador: ${counter}`} />
-
-    {/* exibe o valor do contador */}
-    <h1>{counter}</h1>
-    {/* botão chama a função para incrementar o valor do estado */}
-    <button onClick={handleButtonClick}>Clicar</button>
-
-      <h1>Conteudo da Aplicação</h1>
-    </div>
+    <Home />
   );
 }
 

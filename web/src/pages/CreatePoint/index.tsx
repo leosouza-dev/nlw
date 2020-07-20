6d136d9775793feeacba57e7616916be19f079f9
sarival <sarival@sarival.com.br>
2020-07-20 12:14:01 -0300

    consumindo api - listagem de items

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg'
+import api from '../../services/api';
+
+interface Item{
+    id: number;
+    title: string;
+    image_url: string;
+}
 
 const CreatePoint = () => {
+    // obs: sempre que criarmos um estado para array ou objeto, temos que informar o tipo da variável - usar interface para isso
+    // trabalhando com estado para armazenar valores
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data); //passando os valores para o "items"
+        });
+    }, []);
+
     return (
         <div id="page-create-point">
             <header>
@@ -47,14 +65,51 @@ const CreatePoint = () => {
                 <fieldset>
                     <legend>
                         <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={[-23.5792551, -46.641581]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-23.5792551, -46.641581]}/>
+                    </Map>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                            </select>
+                        </div>
+                    </div>
+
                 </fieldset>
 
                 <fieldset>
                     <legend>
                         <h2>Ítens de coleta</h2>
+                        <span>Selecione um ítem ou mais</span>
                     </legend>
+                    <ul className="items-grid">
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title}/>
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
+                    </ul>
                 </fieldset>
+
+                <button type="submit">Cadastrar ponto de coleta</button>
             </form>
         </div>
     );
